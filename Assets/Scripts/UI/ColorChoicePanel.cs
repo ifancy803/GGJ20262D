998fa0c1777676e77ecbe67a1b286b4cbfa6f972
wjt <1412499544@qq.com>
2026-01-31 00:01:34 +0800

    创建画板，uimanager

--- Assets/Scripts/UI/ColorChoicePanel.cs
+++ Assets/Scripts/UI/ColorChoicePanel.cs
@@ -1,16 +1,8 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorChoicePanel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
