3b830c6384959d65208dcc5878221c92b696c6fa
arataou <3289161156@qq.com>
2026-01-31 21:55:18 +0800

    修bug

--- Assets/Scripts/colorLogic/changeColor.cs
+++ Assets/Scripts/colorLogic/changeColor.cs
@@ -8,7 +8,7 @@ public class changeColor : MonoBehaviour
     void Start()
     {
         // 找到当前场景中所有 Platform
-        platforms = FindObjectsOfTypeAll<Platform>();
+        platforms = FindObjectsOfType<Platform>();
     }
 
     void Update()
