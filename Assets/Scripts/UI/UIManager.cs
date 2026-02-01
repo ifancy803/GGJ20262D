5af6333f7182505b42272a9c06405fa895422382
wjt <1412499544@qq.com>
2026-02-01 15:31:24 +0800

    添加时停fade

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,23 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
     public Image colorChoicePanel;
+    public Image 时停Fadepanel;
     public ColorChoicePanel choicePanel;
     private bool endScale = false;    
     
     [Header("外部变量")] 
     public Color maskColor;
-    
+
+    private void OnEnable()
+    {
+        if (时停Fadepanel == null) 时停Fadepanel = GameObject.FindGameObjectWithTag("时停Panel").transform.GetComponent<Image>();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
@@ -30,6 +38,8 @@ public class UIManager : Singleton<UIManager>
             // 确保不会小于0
             if(Time.timeScale < 0.05f) 
                 Time.timeScale = 0.00f; // 留一点最小值，不要完全为0
+
+            时停Fadepanel.DOFade(0.8f, 0.5f).SetUpdate(true).SetEase(Ease.Flash);
         }
         else if (Input.GetKeyUp(KeyCode.Tab))
         {
@@ -48,6 +58,8 @@ public class UIManager : Singleton<UIManager>
         {
             // 恢复时也使用 unscaledDeltaTime
             Time.timeScale += Time.unscaledDeltaTime * 2f;
+            
+            时停Fadepanel.DOFade(0f, 0.5f).SetUpdate(true).SetEase(Ease.Flash);
             
             if(Time.timeScale >= 1f)
             {
