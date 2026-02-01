4b858d227a9c6b7df46c989680ffc9e53cde225e
ifancy803 <2354587894@qq.com>
2026-02-01 14:40:25 +0800

    修改诸多关卡以及menu

--- Assets/Scripts/UI/LevelImage.cs
+++ Assets/Scripts/UI/LevelImage.cs
@@ -14,7 +14,9 @@ public class LevelImage : MonoBehaviour
     IEnumerator 开始离开()
     {
         yield return new WaitForSeconds(1f);
-        transform.DOMove(new Vector3(transform.position.x, transform.position.y + 300f, transform.position.z), 0.2f).SetEase(Ease.OutBounce);
+        transform.DOMove(new Vector3(transform.position.x, transform.position.y + 300f, transform.position.z), 1f).SetEase(Ease.OutBounce);
         transform.GetComponent<CanvasGroup>().DOFade(0, 1f);
+        yield return new WaitForSeconds(1f);
+        this.gameObject.SetActive(false);
     }
 }
